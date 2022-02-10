5945c9ecb17cf780d294fe442e5d5ac110b00108
Rob Graham <r.ggraham@hotmail.com>
2022-02-10 11:51:21 +0000

    chore(build): Prepare structure for multiple sample projects

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "project_rob"
     startproject "demo"
+    --startproject "sample_runner"
     architecture "x64"
 
     configurations
@@ -14,6 +15,7 @@ odir = "../bin-int/%{cfg.system}-%{cfg.buildcfg}-%{cfg.architecture}"
 ptdir = tdir .. "/%{prj.name}"
 podir = odir .. "/%{prj.name}"
 
+-- Submodules
 include "erika/erika.lua"
 
 -- Change back to our workspace-relative path
@@ -22,106 +24,6 @@ odir = "bin-int/%{cfg.system}-%{cfg.buildcfg}-%{cfg.architecture}"
 ptdir = tdir .. "/%{prj.name}"
 podir = odir .. "/%{prj.name}"
 
-project "demo"
-    location "demo"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "off"
-
-    targetdir(ptdir)
-    objdir(podir)
-    debugdir("data")
-
-    files
-    {
-        "%{prj.name}/include/**.h",
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-    }
-
-    sysincludedirs
-    {
-        "vendor/src",
-        "vendor/include",
-        "%{prj.name}/vendor/include",
-    }
-
-    includedirs
-    {
-        "%{prj.name}/include",
-        "erika/erika/include",
-        "erika/erika/vendor/include",
-    }
-
-    links
-    {
-        "erika",
-    }
-
-	postbuildcommands
-	{
-		"python ../tools/_postbuild.py project=%{prj.name}",
-		"{COPY} %{prj.name}.build.meta ../" .. ptdir,
-        "{COPY} ../" ..tdir .. "/erika/erika.dll ../" ..ptdir
-	}
-
-    filter "system:windows"
-        systemversion "latest"
-        
-        debugdir(ptdir)
-
-        files
-        {
-            "%{prj.name}/vendor/**.natvis",
-        }
-
-        links
-        {
-            
-        }
-
-        defines
-        {
-            "PR_WIN",
-        }
-
-        postbuildcommands
-        {
-            "IF NOT EXIST vendor {MKDIR} vendor",
-            "IF NOT EXIST vendor/bin {MKDIR} vendor/bin",
-            "{COPY} vendor/bin/*.dll ../../" .. ptdir,
-            "IF NOT EXIST ../vendor {MKDIR} ../vendor",
-            "IF NOT EXIST ../vendor/bin {MKDIR} ../vendor/bin",
-            "{COPY} ../vendor/bin/*.dll ../" .. ptdir,
-        }
-
-    filter "system:linux"
-        links
-        {
-            
-        }
-
-        defines
-        {
-            "PR_LINUX",
-        }
-
-    filter "configurations:debug"
-        runtime "Debug"
-        symbols "on"
-
-        defines
-        {
-            "PR_DEBUG",
-        }
-
-    filter "configurations:release"
-        runtime "Release"
-        symbols "off"
-        optimize "on"
-
-		defines
-        {
-            "PR_RELEASE",
-        }
+-- Samples
+include "samples.lua"
+--include "sample_runner.lua"
